ffc11ddefe0006156996687620b73d8962dc821c
Maple <wjxa20152015@gmail.com>
2025-05-04 18:01:44 +0800

    add pipe writer

--- internal/jpyexec/namedpipes.go
+++ internal/jpyexec/namedpipes.go
@@ -12,10 +12,7 @@ import (
 	"github.com/janpfeifer/gonb/gonbui/protocol"
 	"github.com/janpfeifer/gonb/internal/kernel"
 	"github.com/pkg/errors"
-	"io"
 	"k8s.io/klog/v2"
-	"os"
-	"sync"
 )
 
 func init() {
@@ -164,89 +161,4 @@ func (exec *Executor) dispatchInputRequest(req *protocol.InputRequest) {
 	if err != nil {
 		exec.reportCellError(err)
 	}
-}
-
-// openPipeWriter opens `exec.namedPipeWriterPath` and handles its proper closing, and removal of
-// the named pipe when program execution is finished.
-//
-// The doneChan is listened to: when it is closed, it will trigger the listener goroutine to close the pipe,
-// remove it and quit.
-func (exec *Executor) openPipeWriter() {
-	// Synchronize pipe: if it's not opened by the program being executed,
-	// we have to open it ourselves for writing, to avoid blocking
-	// `os.Open` (it waits the other end of the fifo to be opened before returning).
-	// See discussion in:
-	// https://stackoverflow.com/questions/75255426/how-to-interrupt-a-blocking-os-open-call-waiting-on-a-fifo-in-go
-	var muFifo sync.Mutex
-	fifoOpened := false
-
-	go func() {
-		// Clean up after program is over, there are two scenarios:
-		// 1. The executed program opened the pipe: then we just remove the pipePath.
-		// 2. The executed program never opened the pipe: then the other end (goroutine
-		//    below) will be forever blocked on os.Open call.
-		<-exec.doneChan
-		muFifo.Lock()
-		if !fifoOpened {
-			r, err := os.OpenFile(exec.namedPipeWriterPath, os.O_RDONLY, 0600)
-			if err == nil {
-				// Closing it allows the open of the pipe for writing (below) to unblock.
-				_ = r.Close()
-			}
-		}
-		muFifo.Unlock()
-		_ = os.Remove(exec.namedPipeWriterPath)
-	}()
-
-	go func() {
-		klog.V(2).Infof("Opening named pipeWriter in %q", exec.namedPipeWriterPath)
-		if exec.isDone {
-			// In case program execution interrupted early.
-			klog.Warningf("Opening of NamedPipeWriter in %q failed, since program already stopped/crashed", exec.namedPipeWriterPath)
-			return
-		}
-		// Notice that opening the pipe below blocks, until the other end (the go program being executed) opens it
-		// as well.
-		f, err := os.OpenFile(exec.namedPipeWriterPath, os.O_WRONLY, 0600)
-		if err != nil {
-			klog.Warningf("Failed to open pipe (Mkfifo) %q for writing: %+v", exec.namedPipeWriterPath, err)
-			return
-		}
-		klog.V(2).Infof("Opened named pipeWriter in %q", exec.namedPipeWriterPath)
-		muFifo.Lock()
-		exec.pipeWriter = f
-		fifoOpened = true
-		defer muFifo.Unlock()
-
-		// Start polling of the pipeReader.
-		go exec.pollPipeWriterFifo()
-
-		// Wait program execution to finish to close the writer (file and fifo).
-		<-exec.doneChan
-		close(exec.PipeWriterFifo)
-		_ = exec.pipeWriter.Close()
-		_ = os.Remove(exec.namedPipeWriterPath)
-	}()
-}
-
-// pollPipeWriterFifo polls messages from `Executor.PipeWriterFifo` and encodes them to
-// the named pipe writer.
-func (exec *Executor) pollPipeWriterFifo() {
-	encoder := gob.NewEncoder(exec.pipeWriter)
-	klog.V(2).Infof("jpyexec: pollPipeWriterFifo() listening to requests.")
-	for msg := range exec.PipeWriterFifo {
-		if klog.V(2).Enabled() {
-			klog.Infof("jpyexec: encoding %+v to named pipe to cell program", msg)
-		}
-		err := encoder.Encode(msg)
-		if errors.Is(err, io.EOF) || errors.Is(err, io.ErrClosedPipe) || errors.Is(err, os.ErrClosed) {
-			return
-		} else if err != nil {
-			klog.Infof("while writing to cell program, failed to encode message %+v. "+
-				"Communication with cell program broken, widgets won't work properly. "+
-				"You can try re-executing the cell. Error: %+v", msg, err)
-			return
-		}
-	}
-	klog.V(2).Infof("jpyexec: pollPipeWriterFifo() closed.")
 }
